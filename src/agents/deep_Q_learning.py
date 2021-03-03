9aa7e1f199471b9cbbb1a174c8cf53449dba0764
shatha <shatha@kth.se>
2021-03-03 09:27:37 +0100

    first commit

--- src/agents/deep_Q_learning.py
+++ src/agents/deep_Q_learning.py
@@ -172,11 +172,11 @@ class ReplayBuffer():
         k = min(self.batch_size, len(self.memory))
         experiences = random.sample(self.memory, k=k)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None])).float().to(device)
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float64().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float64().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float64().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float64().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None])).float64().to(device)
 
         return (states, actions, rewards, next_states, dones)
 
