c4474493991f750f70c772cf49b0b988d0bc1bed
shatha <shatha@kth.se>
2021-03-03 12:20:32 +0100

    first commit

--- src/agents/deep_Q_learning.py
+++ src/agents/deep_Q_learning.py
@@ -115,7 +115,7 @@ class Agent():
         print("actions: ")
         print(actions)
         print(actions.dtype)
-        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        Q_expected = self.qnetwork_local(states).gather(1, actions.long())
         print("Q_expected type ")
         print(Q_expected.dtype)
         print("Q_targets type ")
