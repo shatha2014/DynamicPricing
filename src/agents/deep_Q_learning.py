647f381398c802d7998fb65f591a5aedd7b0f3e8
shatha <shatha@kth.se>
2021-03-03 09:44:59 +0100

    first commit

--- src/agents/deep_Q_learning.py
+++ src/agents/deep_Q_learning.py
@@ -115,7 +115,7 @@ class Agent():
         print("actions: ")
         print(actions)
         print(actions.dtype)
-        Q_expected = self.qnetwork_local(states).gather(1, actions.double())
+        Q_expected = self.qnetwork_local(states.double()).gather(1, actions)
 
         # Compute the loss
         loss = F.mse_loss(Q_expected, Q_targets)
